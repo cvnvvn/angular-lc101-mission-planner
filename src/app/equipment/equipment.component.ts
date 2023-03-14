0087889b65be82f7fd32465d481f6ef6d62c35a7
cvnvvn <canaan.merrick@gmail.com>
2023-03-13 19:41:23 -0500

    bonus: can add, edit, remove equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  itemBeingEdited: object = null;
+
+  equipment: object[] = [
+    {name: "Habitat dome"},
+    {name: "Drones"},
+    {name: "Food containers"},
+    {name: "Oxygen tanks"}
+  ];
+
+  add(itemName: string) {
+    this.equipment.push({name: itemName});
+  }
+
+  remove(item: object) {
+    let index = this.equipment.indexOf(item);
+    this.equipment.splice(index, 1);
+  }
+
+  edit(item: object) {
+    this.itemBeingEdited = item;
+  }
+
+  save(name: string, item: object) {
+    item['name'] = name;
+    this.itemBeingEdited = null;
+  }
+
+
   constructor() { }
 
   ngOnInit() {
